542114e3004af29dadc479f06ce82e70a55a6ced
brennanharrison <54481933+brennanharrison@users.noreply.github.com>
2019-08-31 15:43:26 -0400

    Add files via upload

---
+++ HarrisonWorkbookAssignmentOneFileTwo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Programmer: Brennan Harrison
+# Date: 08/31/2019
+# Assignment: Workbook Assignment One File Two
+# Description: Import a datafile into Python from https://www.census.gov/support/USACdataDownloads.html
+
+import pandas as pd
+Location = 'datasets/ANC01.xls'
+df = pd.read_excel(Location)
+df.head()
+
+
+# In[ ]:
+
+
+
+
